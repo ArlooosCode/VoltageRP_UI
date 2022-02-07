38c9b3ef0bb0b4cd739596482a228d1f336d0535
K4R00L <99073785+K4R00L@users.noreply.github.com>
2022-02-07 19:03:45 +0100

    Add files via upload

---
+++ [ui]/esx_menu_dialog/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+description 'ESX Menu Dialog'
+
+version '1.1.0'
+
+shared_script '@DiamondCasino/shared.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+
+	'html/css/app.css',
+
+	'html/js/mustache.min.js',
+	'html/js/app.js',
+
+	'html/fonts/pdown.ttf',
+	'html/fonts/bankgothic.ttf'
+}
+
+dependency 'es_extended'
